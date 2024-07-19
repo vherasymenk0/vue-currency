f56c6937d3b3ff9f9b6c3edafc65a90c57a5917a
vherasymenko <manqarium@gmail.com>
2024-07-19 19:12:28 +0300

    [Feature] modified page

---
+++ src/pages/Modified.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import CurrencyList from '../components/CurrencyList.vue'
+import { computed } from 'vue'
+import { useCurrencyStore } from '@/store'
+import MainLayout from '@/layout/MainLayout.vue'
+
+const store = useCurrencyStore()
+const modifiedCurrencies = computed(() => store.state.modifiedCurrencies)
+
+</script>
+<template>
+  <MainLayout>
+    <template #title>Змінені курси</template>
+    <template #content>
+      <CurrencyList :currencies="modifiedCurrencies" />
+    </template>
+  </MainLayout>
+</template>

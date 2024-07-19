4c6573465222ad89c1ad9c7dd64e9e8d58d5e501
vherasymenko <manqarium@gmail.com>
2024-07-19 19:11:59 +0300

    [Feature] home page

---
+++ src/components/CurrencyList.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { Currency } from '@/types/currency.ts'
+
+defineProps<{ currencies: Currency[] }>()
+</script>
+
+<template>
+  <div class="max-w-4xl mx-auto py-8 px-4">
+    <div class="h-96 lg:h-128 overflow-y-auto p-4 border border-gray-200 rounded-lg shadow bg-white">
+      <div v-if="currencies.length === 0">Немає валют</div>
+      <router-link
+        v-for="currency in currencies"
+        :key="currency.cc"
+        :to="`/edit/${currency.cc.toLowerCase()}`"
+        class="text-green-600 font-semibold text-lg border border-gray-300 rounded-lg shadow p-4 mb-4 bg-white hover:bg-customGray transition duration-300 ease-in-out flex justify-between items-center"
+        v-else
+      >
+        <div>
+          <h2 class="text-green-700 font-semibold text-lg">{{ currency.txt }}</h2>
+          <p class="text-gray-600">Курс: {{ currency.rate }} UAH</p>
+        </div>
+        <span class="ml-2">{{ currency.cc }}</span>
+      </router-link>
+    </div>
+  </div>
+</template>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,45 @@
 <script setup lang="ts">
+import { onMounted, computed, reactive, ref } from 'vue'
+import axios from 'axios'
+import Pagination from '@/components/Pagination.vue'
+import CurrencyList from '@/components/CurrencyList.vue'
+import { useRoute } from 'vue-router'
+import { Currency } from '@/types/currency.ts'
+import MainLayout from '@/layout/MainLayout.vue'
+import Loader from '@/components/Loader.vue'
+
+const route = useRoute()
+const isLoading = ref(false)
+const currentPage = computed(() => Number(route.query?.page) || 1)
+const currentDate = new Intl.DateTimeFormat('uk', {
+  dateStyle: 'full',
+  timeZone: 'Europe/Kyiv'
+}).format(new Date)
+
+const data = reactive<Currency[]>([])
+const pageData = computed(() => {
+  const start = (currentPage.value - 1) * 10
+  const end = start + 10
+  return data.slice(start, end)
+})
+
+onMounted(async () => {
+  isLoading.value = true
+  const response = await axios.get('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json')
+  data.push(...response.data)
+  isLoading.value = false
+})
 </script>
 
 <template>
-  <div>
-    Home page
-  </div>
+  <MainLayout>
+    <template #title>Курси валют НБУ станом на {{ currentDate }}</template>
+    <template #content>
+      <Loader v-if="isLoading" />
+      <template v-else>
+        <CurrencyList :currencies="pageData" />
+        <Pagination :totalItems="data.length" :page="currentPage" />
+      </template>
+    </template>
+  </MainLayout>
 </template>
-
-<style scoped>
-</style>

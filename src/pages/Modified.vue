d32fd67396a4f7a9ebd02a9ca1cb92816b875413
vherasymenko <manqarium@gmail.com>
2024-07-19 19:15:13 +0300

    [Fix] removed store issues

--- src/pages/Modified.vue
+++ src/pages/Modified.vue
@@ -1,11 +1,8 @@
 <script setup lang="ts">
-import CurrencyList from '../components/CurrencyList.vue'
-import { computed } from 'vue'
-import { useCurrencyStore } from '@/store'
 import MainLayout from '@/layout/MainLayout.vue'
+import CurrencyList from '@/components/CurrencyList.vue'
 
-const store = useCurrencyStore()
-const modifiedCurrencies = computed(() => store.state.modifiedCurrencies)
+const modifiedCurrencies = []
 
 </script>
 <template>

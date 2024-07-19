22a81d11cc6760ff284ba6351ee992dd50ff6a69
vherasymenko <manqarium@gmail.com>
2024-07-19 19:11:35 +0300

    [Feature] setup router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,14 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, RouterOptions } from 'vue-router'
 import Home from '@/pages/Home.vue'
+import Edit from '@/pages/Edit.vue'
 import Search from '@/pages/Search.vue'
+import Modified from '@/pages/Modified.vue'
 
-const routes = [
+const routes: RouterOptions['routes'] = [
   { path: '/', name: 'Home', component: Home },
-  { path: '/search', name: 'SearchRates', component: Search }
+  { path: '/modified', name: 'Modified', component: Modified },
+  { path: '/search', name: 'Search', component: Search },
+  { path: '/edit/:cc', name: 'Edit', component: Edit }
 ]
 
 const router = createRouter({
---
+++ src/router/route.paths.ts
@@ -0,0 +1,14 @@
+export const ROUTE_PATHS = [
+  {
+    to: '/',
+    label: 'Головна'
+  },
+  {
+    to: '/modified',
+    label: 'Змінені курси'
+  },
+  {
+    to: '/search',
+    label: 'Пошук курсу'
+  }
+] as const
